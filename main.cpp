ab41bf2919ac30f860232a61961e75ea1d447ff3
sepfish <trskybr@gmail.com>
2023-12-05 18:08:43 -0500

    IT COMPILES!

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
+
+
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
 #include "lib/include/AudioPlayer.h"
 
 
@@ -6,5 +10,6 @@ int main(int argc, char** argv) {
     AudioPlayer a(argv[1]);
     a.playSong();
 
+    cv::VideoCapture cap(argv[2]);
     return 0;
 }
